888e5b5614fc9e0ada83391b40e6bf3586abd28d
Sindre Moen <sindrenm@gmail.com>
2024-07-18 17:21:01 +0200

    Create new project

---
+++ app/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.sindrenm.repros.typednavigationruntimecrash"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.sindrenm.repros.typednavigationruntimecrash"
+
+        minSdk = 24
+        targetSdk = 35
+
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core)
+    implementation(libs.google.material)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Typed Navigation Runtime Crash"
+include(":app")
